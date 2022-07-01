be682918e0e5c056d6b943dbd9a9209b6a338bbe
unknown <austinscottwebb@icloud.com>
2022-07-01 17:16:59 -0400

    adding place controller

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
-const express = require('express')
+const express = require ('express')
 const app = express()
+
+
 
 app.use('/places', require('./controllers/places'))
 
@@ -13,3 +15,4 @@ app.get('*', (req, res) => {
 })
 
 app.listen(process.env.PORT)
+
