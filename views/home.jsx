5d2e2b93e6d49e484a75179ba1b4a04c857c1746
unknown <austinscottwebb@icloud.com>
2022-07-04 23:00:49 -0400

    adding the styles

--- views/home.jsx
+++ views/home.jsx
@@ -1,11 +1,18 @@
 const React = require('react')
 const Def = require('./default')
 
+
+
 function home () {
     return (
       <Def>
           <main>
+   
               <h1>HOME</h1>
+              <link rel = 'stylesheet' href='/css/style.css'></link>
+                       <a href="/places">
+  <button className="btn-primary">Places Page</button>
+</a>
           </main>
       </Def>
     )
